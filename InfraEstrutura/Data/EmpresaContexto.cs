89107d2fee5d545864c3fb9b831cb9968c9afd31
gustavo leal <guuh.leal@hotmail.com>
2025-08-18 21:42:01 -0300

    FirstComitt

---
+++ InfraEstrutura/Data/EmpresaContexto.cs
@@ -0,0 +1,45 @@
+﻿using Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfraEstrutura.Data
+{
+    public class EmpresaContexto : DbContext
+
+    {
+        public EmpresaContexto(DbContextOptions<EmpresaContexto> opcoes) : base(opcoes)
+        {
+        }
+
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Categoria>(builder => {
+                builder.Property(p => p.Descricao).IsRequired().HasMaxLength(150);//descrição obrigatoria
+                builder.ToTable("Categoria");//nome da tabela
+                builder.HasKey(p => p.Id);
+                
+
+            });
+            modelBuilder.Entity<Produto>(builder =>
+            {
+                builder.ToTable("produto");
+                builder.HasKey(p => p.Id);
+                builder.Property(p => p.Descricao).IsRequired().HasMaxLength(150);
+                builder.Property(p => p.Valor).HasPrecision(8, 2).IsRequired();
+                builder.Property(p => p.Quantidade).IsRequired();
+                builder.HasOne(p => p.Categoria).WithMany(p => p.produtos).HasForeignKey(p => p.IdCategoria).OnDelete(DeleteBehavior.Restrict);
+            });
+        }
+    }
+
+}
---
+++ InfraEstrutura/Migrations/20250819002543_init.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InfraEstrutura.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categoria",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descricao = table.Column<string>(type: "nvarchar(151)", maxLength: 150, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categoria", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "produto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descricao = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Valor = table.Column<decimal>(type: "decimal(8,2)", precision: 8, scale: 2, nullable: false),
+                    Quantidade = table.Column<int>(type: "int", nullable: false),
+                    IdCategoria = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_produto", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_produto_Categoria_IdCategoria",
+                        column: x => x.IdCategoria,
+                        principalTable: "Categoria",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_produto_IdCategoria",
+                table: "produto",
+                column: "IdCategoria");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "produto");
+
+            migrationBuilder.DropTable(
+                name: "Categoria");
+        }
+    }
+}
